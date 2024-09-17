2b2f4ff2c124d4a81e1179ba056513fcf4b4aa33
RuslanGon <ruslan441006@gmail.com>
2024-09-17 12:48:32 +0300

    wer

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store.js'
+import { persistor } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store.js'
+
 
 
 createRoot(document.getElementById("root")).render(
