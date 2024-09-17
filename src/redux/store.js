157a123d6fa1ffc7dff3778ac81f57edc47edd95
RuslanGon <ruslan441006@gmail.com>
2024-09-17 11:48:03 +0300

    wre

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
-import { combineReducers, createStore } from "@reduxjs/toolkit";
+import {  configureStore } from "@reduxjs/toolkit";
 import { productsReducer } from "./products/productsReducer.js";
 
-const rootReducer = combineReducers({
-products: productsReducer
-})
-
-export const store = createStore(rootReducer)
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+}); 
