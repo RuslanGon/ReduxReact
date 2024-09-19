d8a678042e588e6b7171f57d4422384c22eb6947
RuslanGon <ruslan441006@gmail.com>
2024-09-19 17:02:39 +0300

    wer

--- src/page/RegistrationPage.jsx
+++ src/page/RegistrationPage.jsx
@@ -1,9 +1,63 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
+
+const registorSchema = Yup.object({
+  name: Yup.string().required("name is required"),
+  email: Yup.string().email("Invalid email format").required("email is required"),
+  password: Yup.string().required("password is required"),
+});
+
+const FORM_INITIAL_VALUES = {
+  name: "",
+  email: "",
+  password: ''
+};
 
 const RegistrationPage = () => {
-  return (
-    <div>RegistrationPage</div>
-  )
-}
+  const handleSubmit = (values, actions) => {
+    console.log(values);;
+    actions.resetForm();
+  };
 
-export default RegistrationPage
+  return (
+    <Formik
+      initialValues={FORM_INITIAL_VALUES}
+      onSubmit={handleSubmit}
+      validationSchema={registorSchema}
+    >
+      <Form>
+        <h2>Registor new user</h2>
+        <br />
+        <label>
+          <span>name:</span>
+          <br />
+          <Field type="text" name="name" placeholder="name" />
+          <ErrorMessage name="name" component="span" />
+        </label>
+        <br />
+        <br />
+        <label>
+          <span>email:</span>
+          <br />
+          <Field type="email" name="email" placeholder="email" />
+          <ErrorMessage name="email" component="span" />
+          <br />
+        </label>
+        <br />
+        <label>
+          <span>password:</span>
+          <br />
+          <Field type="text" name="password" placeholder="password" />
+          <ErrorMessage name="password" component="span" />
+          <br />
+        </label>
+        <br />
+      <button type="submit">Create new user 👱</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default RegistrationPage;
+
